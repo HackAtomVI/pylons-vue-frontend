939dfe2599e4cbe734651c54d30083054fd6bdee
Patrick <patrick.wieth@goooglemail.com>
2021-11-21 20:46:50 +0100

    updated port configs

--- src/App.vue
+++ src/App.vue
@@ -38,14 +38,34 @@ export default {
     },
   },
   async created() {
-    await this.$store.dispatch('common/env/init', {
-      apiNode: 'http://46.166.140.172:1317/',
-      rpcNode: 'http://46.166.140.172:26657/',
+    let local = {
+      apiNode: 'http://localhost:1317',
+      rpcNode: 'http://localhost:26657',
       wsNode: 'ws://api.testnet.pylons.tech:26657/websocket',
       chainId: 'pylons',
       addrPrefix: 'pylo',
       sdkVersion: 'Stargate',
-    })
+      getTXApi: 'http://localhost:26657/tx?hash=0x',
+    }
+    let pylonsserver = {
+      apiNode: 'http://46.166.140.172:1317',
+      rpcNode: 'http://46.166.140.172:26657',
+      wsNode: 'ws://api.testnet.pylons.tech:26657/websocket',
+      chainId: 'pylons',
+      addrPrefix: 'pylo',
+      sdkVersion: 'Stargate',
+      getTXApi: 'http://localhost:26657/tx?hash=0x',
+    }
+    let shameserver = {
+      apiNode: 'http://v2202008103543124756.megasrv.de:1318',
+      rpcNode: 'http://v2202008103543124756.megasrv.de:26659',
+      wsNode: 'ws://v2202008103543124756.megasrv.de:26659/websocket',
+      chainId: 'pylons',
+      addrPrefix: 'pylo',
+      sdkVersion: 'Stargate',
+      getTXApi: 'http://v2202008103543124756.megasrv.de:26659/tx?hash=0x',
+    }
+    await this.$store.dispatch('common/env/init', shameserver)
     this.initialized = true
   },
   errorCaptured(err) {
